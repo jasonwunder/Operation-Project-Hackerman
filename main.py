41ffec14fb77f9a24eceb07e4c7fb29e909a04ee
Jason Wunder <jasonwunder94@gmail.com>
2022-02-23 16:07:04 -0800

    Started to code project

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-#sounds good sweet
-#yes it does
-# set up git on laptop
+import sys
+import os
+
+# check the version of python that the user is using. is older than version 3, print an error message.
+if sys.version_info < 3:
+    raise Exception("Sorry, Operation Project Hackerman only works with Python 3 ")
+
