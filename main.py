b23fc6f8376b16cda5c4dd3f119684e1b5d0e7e8
Jason Wunder <jasonwunder94@gmail.com>
2022-02-23 16:55:06 -0800

    Created a version checker and user generated list

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
+from curses import termattrs
 import sys
 import os
 
-# check the version of python that the user is using. is older than version 3, print an error message.
-if sys.version_info < 3:
-    raise Exception("Sorry, Operation Project Hackerman only works with Python 3 ")
+# check the version of python that the user is using. if older than version 3, print an error message.
+if (sys.version_info < (3, 0)):
+    print('Error: Operation Project Hackerman only works with Python 3')
+    sys.exit(1)
+
+#if sys.version_info < 3:
+#    raise Exception("Sorry, Operation Project Hackerman only works with Python 3 ")
+
+# Create variables for what needs to be used in the password generator.
+uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lowercase_letters = uppercase_letters.lower()
+digits = "0123456789"
+symbols = "!#$%&()*+,-./:;<=>?@[\\]^_`{|}~"
+
+# Create a list of the user provied terms
+term_list = []
+# Input number of elements
+n = int(input("Enter the number of terms you are using: "))
+
+for i in range(0, n):
+    print("Enter term number-{}: ".format(i + 1))
+    # Add the element to the list
+    term_list.append(input())
+print("The entered terms are: \n", term_list)
+
 
