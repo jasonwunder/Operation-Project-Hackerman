edae42e57146e64076b8001d94a1d76224d8dc2c
Jason Wunder <jasonwunder94@gmail.com>
2022-02-24 18:28:22 -0800

    Added promts

--- test_code.py
+++ test_code.py
@@ -13,24 +13,32 @@ if (sys.version_info < (3, 0)):
 #if sys.version_info < 3:
 #    raise Exception("Sorry, Operation Project Hackerman only works with Python 3 ")
 
-# Create a functionf for the user provied terms
+# Create a function for the user provied terms
 term_list = []
-# Input number of elements
+first_name = input("Enter targets first name: ")
+term_list.append(first_name)
+
+last_name = input("Enter targets last name: ")
+term_list.append(last_name)
+
+pet = input("Does the target have a pet? [y/n]")
+if pet == "y":
+    pet_name = input("Enter pets name: ")
+    term_list.append(pet_name)
+
+dob = input("Do you know the targets DOB? [y/n] ")
+if dob == "y":
+    dob_answ = input("Enter DOB in either of the following formats: [mmddyy] [mmddyyyy] [yy] [yyyy]: ")
+    term_list.append(dob_answ)
+else:
+    pass   
+print("The terms you entered are: ", term_list)
 n = int(input("Enter the number of terms you are using: "))
 for i in range(0, n):
     print("Enter term number-{}: ".format(i + 1))
     # Add the element to the list
     term_list.append(input())
-print("The entered terms are: \n", term_list)   
-dob = input("Do you know the targets DOB? [y/n] ")
-if dob == "y":
-    answer = input("Enter DOB in either of the following formats: [mmddyy] [mmddyyyy] [yy] [yyyy]: ")
-    term_list.append(answer)
-else:
-    pass   
-print("The terms you entered are: ", term_list)
-
-
+print("The entered terms are: \n", term_list)
 # Have user validate if information is correct. If it is not have them enter the terms again.
 
 # create a function that looks for date formats within the list
