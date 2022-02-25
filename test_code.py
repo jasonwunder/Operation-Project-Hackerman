a9970a171ffd79ed529ced228555f3bcf6033c1a
Jason Wunder <jason.jw@protonmail.com>
2022-02-24 20:04:43 -0800

    created function for usr generated list

--- test_code.py
+++ test_code.py
@@ -11,40 +11,46 @@ if (sys.version_info < (3, 5)):
     sys.exit(1)
 
 # Create a function for the user provied terms
-term_list = []
-first_name = input("Enter targets first name: ")
-term_list.append(first_name)
+def term_list():
+    term_list = []
+    first_name = input("Enter targets first name: ")
+    term_list.append(first_name)
 
-last_name = input("Enter targets last name: ")
-term_list.append(last_name)
+    last_name = input("Enter targets last name: ")
+    term_list.append(last_name)
 
-nickname = input("Does the target have a known nickname? [y/n]")
-if nickname == "y":
-    nn = input("Enter the targets nickname: ")
-    term_list.append(nn)
-else:
-    pass
+    nickname = input("Does the target have a known nickname? [y/n]")
+    if nickname == "y":
+        nn = input("Enter the targets nickname: ")
+        term_list.append(nn)
+    else:
+        pass
 
-pet = input("Does the target have a pet? [y/n]")
-if pet == "y":
-    pet_name = input("Enter pets name: ")
-    term_list.append(pet_name)
+    pet = input("Does the target have a pet? [y/n]")
+    if pet == "y":
+        pet_name = input("Enter pets name: ")
+        term_list.append(pet_name)
 
-dob = input("Do you know the targets DOB? [y/n] ")
-if dob == "y":
-    dob_answ = input("Enter DOB in either of the following formats: [mmddyy] [mmddyyyy] [yy] [yyyy]: ")
-    term_list.append(dob_answ)
-else:
-    pass
-print("The terms you entered are: ", term_list)
-more_terms = input("Are there any other terms you want to add? [y/n]")
-if more_terms == "y":
-    n = int(input("Enter the number of terms: "))
-    for i in range(0, n):
-        print("Enter term number-{}: ".format(i + 1))
-        # Add the element to the list
-        term_list.append(input())
-print("The entered terms are: \n", term_list)
+    dob = input("Do you know the targets DOB? [y/n] ")
+    if dob == "y":
+        dob_answ = input("Enter DOB in either of the following formats: [mmddyy] [mmddyyyy] [yy] [yyyy]: ")
+        term_list.append(dob_answ)
+    else:
+        pass
+    print("The terms you entered are: ", term_list)
+    more_terms = input("Are there any other terms you want to add? [y/n]")
+    if more_terms == "y":
+        n = int(input("Enter the number of terms: "))
+        for i in range(0, n):
+            print("Enter term number-{}: ".format(i + 1))
+            # Add the element to the list
+            term_list.append(input())
+    print("The entered terms are: \n", term_list)
+
+
+
+
+
 # Have user validate if information is correct. If it is not have them enter the terms again.
 
 # create a function that looks for date formats within the list
