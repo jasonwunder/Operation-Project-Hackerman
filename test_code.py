31ef1e39376a07c98935b07c6fb488261f4d39ea
Jason Wunder <jason.jw@protonmail.com>
2022-02-26 12:20:22 -0800

    added functions

--- test_code.py
+++ test_code.py
@@ -1,11 +1,11 @@
 ###################################################################################################
 ############# THIS IS NOT FOR MAIN PRODUCT#################
 ###################################################################################################
+from email.utils import collapse_rfc2231_value
 import sys
 import os
 import random
 import itertools
-from xml.dom.minidom import Element
 
 # check the version of python that the user is using. if older than version 3, print an error message.
 if (sys.version_info < (3, 5)):
@@ -17,19 +17,27 @@ if (sys.version_info < (3, 5)):
 
 def usr_input():
     term_list = []
+    # Check for first name
     first_name = input("Enter targets first name: ")
     term_list.append(first_name)
-
+    # Check for middle name
+    middle_name = input("Do you know the targets middle name? [y/n] ")
+    if middle_name == "y":
+        mn_answ = input("Enter the targets middle name: ")
+        term_list.append(mn_answ)
+    else:
+        pass
+    # Check for last name
     last_name = input("Enter targets last name: ")
     term_list.append(last_name)
-
+    # Check for nickname
     nickname = input("Does the target have a known nickname? [y/n]")
     if nickname == "y":
         nn_answ = input("Enter the targets nickname: ")
         term_list.append(nn_answ)
     else:
         pass
-
+    # Check for pet
     pet = input("Does the target have a pet? [y/n]")
     if pet == "y":
         pet_name = input("Enter pets name: ")
@@ -47,6 +55,20 @@ def usr_input():
         term_list.append(year_short)
     else:
         pass
+    # Check for phone number area code
+    area_code = input("Do you know the targets phone number? [y/n]")
+    if area_code == "y":
+        ac_answ = input("Enter the area code of the phone number: [###] ")
+        term_list.append(ac_answ)
+    else:
+        pass
+    # Check for favorite sports team
+    sports_team = input("Do you know the targets favorite sports team? [y/n]")
+    if sports_team == "y":
+        st_answ = input("Enter the team: ")
+        term_list.append(st_answ)
+    else:
+        pass
     print("The terms you entered are: ", term_list)
     # Check for more terms
     more_terms = input("Are there any other terms you want to add? [y/n]")
@@ -56,9 +78,11 @@ def usr_input():
             print("Enter term number {}: ".format(i + 1))
             # Add the element to the list
             term_list.append(input())
-        return term_list
-    print("The entered terms are: \n",term_list)
-    #call other functions for use of term_list
+    else:
+        pass
+    print("The entered terms are: \n", term_list)
+
+    # call other functions for use of term_list
     term_combine(term_list)
     underscore(term_list)
     dot(term_list)
@@ -69,72 +93,130 @@ def usr_input():
     minus_sign(term_list)
     percent_sign(term_list)
     carrot_sign(term_list)
+    both_cap(term_list)
+    cap2(term_list)
 
-# Create function that takes in master list and combines elements together 
+# Create function that takes in master list and combines elements together
+
+
 def term_combine(term_list):
     combine_list = []
-    combine_list = list(''.join(entry) for entry in itertools.product(term_list, repeat=2))
+    combine_list = list(''.join(entry)
+                        for entry in itertools.product(term_list, repeat=2))
     print(combine_list)
     cap_char1(combine_list)
 
-# Create a list that takes in combine_list a caps the first letter of the element 
+# Create a function that caps both words
+
+
+def both_cap(term_list):
+    cap_list = term_list.copy()
+    cap_both_list = []
+    for elem in cap_list:
+        cap_both_list.append(elem.capitalize())
+    cap_both_list = list(''.join(entry)
+                         for entry in itertools.product(cap_both_list, repeat=2))
+    print(cap_both_list)
+
+# Create a function that caps the second word in the list
+
+
+def cap2(term_list):
+    term_list_copy = term_list.copy()
+    cap2_list = []
+    for elem in term_list_copy:
+        cap2_list.append(elem.capitalize())
+    cap2_list = list(''.join(entry)
+                     for entry in itertools.product(cap2_list, repeat=2))
+    cap2_list = cap2_list[0].lower() + cap2_list[1:]
+    print(cap2_list)
+
+# Create a list that takes in combine_list a caps the first letter of the element
+
+
 def cap_char1(combine_list):
     cap1_list = []
     for elem in combine_list:
         cap1_list.append(elem.capitalize())
     print(cap1_list)
 
-# Create a function that adds "_" between the two terms 
+# Create a function that adds "_" between the two terms
+
+
 def underscore(term_list):
     underscore_list = []
-    underscore_list = list('_'.join(entry) for entry in itertools.product(term_list, repeat=2))
+    underscore_list = list('_'.join(entry)
+                           for entry in itertools.product(term_list, repeat=2))
     print(underscore_list)
 # Create a function that adds "." between the two terms
+
+
 def dot(term_list):
     dot_list = []
-    dot_list = list('.'.join(entry) for entry in itertools.product(term_list, repeat=2))
+    dot_list = list('.'.join(entry)
+                    for entry in itertools.product(term_list, repeat=2))
     print(dot_list)
 # Create a function that adds a "*" between the two terms
+
+
 def star(term_list):
     star_list = []
-    star_list = list('*'.join(entry) for entry in itertools.product(term_list, repeat=2))
+    star_list = list('*'.join(entry)
+                     for entry in itertools.product(term_list, repeat=2))
     print(star_list)
 # Create a function that adds a "!" between the two terms
+
+
 def explanation(term_list):
     explanation_list = []
-    explanation_list = list('!'.join(entry) for entry in itertools.product(term_list, repeat=2))
+    explanation_list = list('!'.join(entry)
+                            for entry in itertools.product(term_list, repeat=2))
     print(explanation_list)
 # Create a function that adds a "$" between the two terms
+
+
 def dollar(term_list):
     dollar_list = []
-    dollar_list = list('$'.join(entry) for entry in itertools.product(term_list, repeat=2))
+    dollar_list = list('$'.join(entry)
+                       for entry in itertools.product(term_list, repeat=2))
     print(dollar_list)
 # Create a function that adds a "@" between the two terms
+
+
 def at_sign(term_list):
     at_sign_list = []
-    at_sign_list = list('@'.join(entry) for entry in itertools.product(term_list, repeat=2))
+    at_sign_list = list('@'.join(entry)
+                        for entry in itertools.product(term_list, repeat=2))
     print(at_sign_list)
 # Create a function that adds a "-" between the two terms
+
+
 def minus_sign(term_list):
     minus_sign_list = []
-    minus_sign_list = list('-'.join(entry) for entry in itertools.product(term_list, repeat=2))
+    minus_sign_list = list('-'.join(entry)
+                           for entry in itertools.product(term_list, repeat=2))
     print(minus_sign_list)
 # Create a function that adds a "%" between the two terms
+
+
 def percent_sign(term_list):
     percent_sign_list = []
-    percent_sign_list = list('%'.join(entry) for entry in itertools.product(term_list, repeat=2))
+    percent_sign_list = list('%'.join(entry)
+                             for entry in itertools.product(term_list, repeat=2))
     print(percent_sign_list)
 # Create a function that adds a "^" between the two terms
+
+
 def carrot_sign(term_list):
     carrot_sign_list = []
-    carrot_sign_list = list('^'.join(entry) for entry in itertools.product(term_list, repeat=2))
+    carrot_sign_list = list('^'.join(entry)
+                            for entry in itertools.product(term_list, repeat=2))
     print(carrot_sign_list)
 
-# Create a function that caps the second word in the list 
 
 # Create a function that adds syms to end
 
-# Create a function that adds a sym to begaining 
+# Create a function that adds a sym to begaining
 
 # Create a function that changes "a" to "@"
 
@@ -145,37 +227,29 @@ def carrot_sign(term_list):
 # Create a function that changes "s" to "$"
 
 # Create a function that changes "b" to "9"
-    
+
 # Create a function that changes "l" to "1"
 
 # Create a function that changes "r" to "4"
 
-# combine all the lists togehter into one big list 
+# Create a function that changes "i" to "1"
 
-# Create a new list that caps the first letter in the elements of the list 
+# Create a function that changes "t" to "7"
 
-# Combine the lists again 
+# Create a function that adds the a number 0-9 to the end
+
+# combine all the lists togehter into one big list
+
+# Create a new list that caps the first letter in the elements of the list
+
+# Combine the lists again
+
+# Go through that final list and get rid of and element that has less than 6 charaters in it.
 
 # output that list onto a file as a wordlist
 
 
 usr_input()
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 '''
