dabf014e80be41c54e033aac8a97a3d40a7e2521
Jason Wunder <jasonwunder94@gmail.com>
2022-02-27 14:47:29 -0800

    Made tuple functions & starting compliling lists

--- test_code.py
+++ test_code.py
@@ -1,19 +1,22 @@
 ###################################################################################################
 ############# THIS IS NOT FOR MAIN PRODUCT#################
 ###################################################################################################
+from doctest import master
 from email.utils import collapse_rfc2231_value
 import sys
 import os
 import random
 import itertools
 
+
 # check the version of python that the user is using. if older than version 3, print an error message.
 if (sys.version_info < (3, 5)):
     print('Error: Operation Project Hackerman only works with Python 3')
     sys.exit(1)
-
+# Create a master list that every function will append to to make one big list
+master_list = []
+master_list2 = []
 # Create a function for the user provied terms
-
 def usr_input():
     term_list = []
     # Check for first name
@@ -83,33 +86,56 @@ def usr_input():
 
     # call other functions for use of term_list
     term_combine(term_list)
-    underscore(term_list)
-    dot(term_list)
-    star(term_list)
-    explanation(term_list)
-    dollar(term_list)
-    at_sign(term_list)
-    minus_sign(term_list)
-    percent_sign(term_list)
-    carrot_sign(term_list)
+    # underscore(term_list)
+    # dot(term_list)
+    # star(term_list)
+    # explanation(term_list)
+    # dollar(term_list)
+    # at_sign(term_list)
+    # minus_sign(term_list)
+    # percent_sign(term_list)
+    # carrot_sign(term_list)
     both_cap(term_list)
     # cap2(term_list)
-    a_change_at(term_list, "a", "@")
-    zero_change(term_list)
-    three_change(term_list)
-    dollar_sign(term_list)
-    nine_change(term_list)
-    one_change(term_list)
-    four_change(term_list)
-    one_i_change(term_list)
-    seven_change(term_list)
+    # a_change_at(term_list, "a", "@")
+    # zero_change(term_list)
+    # three_change(term_list)
+    # dollar_sign(term_list)
+    # nine_change(term_list)
+    # one_change(term_list)
+    # four_change(term_list)
+    # one_i_change(term_list)
+    # seven_change(term_list)
+    sep_word(term_list, "!")
+    sep_word(term_list, "_")
+    sep_word(term_list, "%")
+    sep_word(term_list, "$")
+    sep_word(term_list, "@")
+    sep_word(term_list, ".")
+    sep_word(term_list, "-")
+    sep_word(term_list, "*")
+    sep_word(term_list, "&")
+    change_char(term_list, "a", "@")
+    change_char(term_list, "s", "$")
+    change_char(term_list, "e", "3")
+    change_char(term_list, "o", "0")
+    change_char(term_list, "b", "9")
+    change_char(term_list, "l", "1")
+    change_char(term_list, "r", "4")
+    change_char(term_list, "t", "7")
+    change_char(term_list, "i", "1")
+
+
+
 
 # Create function that takes in master list and combines elements together
 def term_combine(term_list):
     combine_list = []
     combine_list = list(''.join(entry)
         for entry in itertools.product(term_list, repeat=2))
-    print(combine_list)
+    for elem in combine_list:
+        master_list.append(elem)
+    # print(combine_list)
     cap_char1(combine_list)
 
 # Create a function that caps both words
@@ -120,7 +146,9 @@ def both_cap(term_list):
         cap_both_list.append(elem.capitalize())
     cap_both_list = list(''.join(entry)
         for entry in itertools.product(cap_both_list, repeat=2))
-    print(cap_both_list)
+    for elem in cap_both_list:
+        master_list.append(elem)
+    # print(cap_both_list)
 
 # Create a function that caps the second word in the list
 # def cap2(term_list):
@@ -138,119 +166,143 @@ def cap_char1(combine_list):
     cap1_list = []
     for elem in combine_list:
         cap1_list.append(elem.capitalize())
-    print(cap1_list)
+    for elem in cap1_list:
+        master_list.append(elem)
+    # print(cap1_list)
 
 # Shorten into on function with a tuple and for loop
+def sep_word(term_list, value):
+    sep_list = []
+    sep_list = list(value.join(entry)
+        for entry in itertools.product(term_list, repeat=2))
+    for elem in sep_list:
+        master_list.append(elem)
+    
+
+    
+
 # Create a function that adds "_" between the two terms
-def underscore(term_list):
-    underscore_list = []
-    underscore_list = list('_'.join(entry)
-        for entry in itertools.product(term_list, repeat=2))
-    print(underscore_list)
+# def underscore(term_list):
+#     underscore_list = []
+#     underscore_list = list('_'.join(entry)
+#         for entry in itertools.product(term_list, repeat=2))
+#     print(underscore_list)
 # Create a function that adds "." between the two terms
-def dot(term_list):
-    dot_list = []
-    dot_list = list('.'.join(entry)
-        for entry in itertools.product(term_list, repeat=2))
-    print(dot_list)
+# def dot(term_list):
+#     dot_list = []
+#     dot_list = list('.'.join(entry)
+#         for entry in itertools.product(term_list, repeat=2))
+#     print(dot_list)
 # Create a function that adds a "*" between the two terms
-def star(term_list):
-    star_list = []
-    star_list = list('*'.join(entry)
-        for entry in itertools.product(term_list, repeat=2))
-    print(star_list)
+# def star(term_list):
+#     star_list = []
+#     star_list = list('*'.join(entry)
+#         for entry in itertools.product(term_list, repeat=2))
+#     print(star_list)
 # Create a function that adds a "!" between the two terms
-def explanation(term_list):
-    explanation_list = []
-    explanation_list = list('!'.join(entry)
-        for entry in itertools.product(term_list, repeat=2))
-    print(explanation_list)
+# def explanation(term_list):
+#     explanation_list = []
+#     explanation_list = list('!'.join(entry)
+#         for entry in itertools.product(term_list, repeat=2))
+#     print(explanation_list)
 # Create a function that adds a "$" between the two terms
-def dollar(term_list):
-    dollar_list = []
-    dollar_list = list('$'.join(entry)
-        for entry in itertools.product(term_list, repeat=2))
-    print(dollar_list)
+# def dollar(term_list):
+#     dollar_list = []
+#     dollar_list = list('$'.join(entry)
+#         for entry in itertools.product(term_list, repeat=2))
+#     print(dollar_list)
 # Create a function that adds a "@" between the two terms
-def at_sign(term_list):
-    at_sign_list = []
-    at_sign_list = list('@'.join(entry)
-        for entry in itertools.product(term_list, repeat=2))
-    print(at_sign_list)
+# def at_sign(term_list):
+#     at_sign_list = []
+#     at_sign_list = list('@'.join(entry)
+#         for entry in itertools.product(term_list, repeat=2))
+#     print(at_sign_list)
 # Create a function that adds a "-" between the two terms
-def minus_sign(term_list):
-    minus_sign_list = []
-    minus_sign_list = list('-'.join(entry)
-        for entry in itertools.product(term_list, repeat=2))
-    print(minus_sign_list)
+# def minus_sign(term_list):
+#     minus_sign_list = []
+#     minus_sign_list = list('-'.join(entry)
+#         for entry in itertools.product(term_list, repeat=2))
+#     print(minus_sign_list)
 # Create a function that adds a "%" between the two terms
-def percent_sign(term_list):
-    percent_sign_list = []
-    percent_sign_list = list('%'.join(entry)
-        for entry in itertools.product(term_list, repeat=2))
-    print(percent_sign_list)
+# def percent_sign(term_list):
+#     percent_sign_list = []
+#     percent_sign_list = list('%'.join(entry)
+#         for entry in itertools.product(term_list, repeat=2))
+#     print(percent_sign_list)
 # Create a function that adds a "^" between the two terms
-def carrot_sign(term_list):
-    carrot_sign_list = []
-    carrot_sign_list = list('^'.join(entry)
-        for entry in itertools.product(term_list, repeat=2))
-    print(carrot_sign_list)
+# def carrot_sign(term_list):
+#     carrot_sign_list = []
+#     carrot_sign_list = list('^'.join(entry)
+#         for entry in itertools.product(term_list, repeat=2))
+#     print(carrot_sign_list)
 
 # Shorten into on function with a tuple and for loop
-# Create a function that changes "a" to "@"
-def a_change_at(term_list, old_value, new_value):
-    new_list = term_list.copy()
+def change_char(term_list, old_value, new_value):
+    # new_char = [(old_value, new_value)]
+    new_list = master_list.copy()
+    change_char_list = []
     for idx, value in enumerate(new_list):
         new_list[idx] = value.replace(old_value, new_value)
-    print(new_list)
-# Create a function that changes "o" to "0"
-def zero_change(term_list):
-    zero_list = term_list.copy()
-    for idx, value in enumerate(zero_list):
-        zero_list[idx] = value.replace("o", "0")
-    print(zero_list)
-# Create a function that changes "e" to "3"
-def three_change(term_list):
-    three_list = term_list.copy()
-    for idx, value in enumerate(three_list):
-        three_list[idx] = value.replace("e", "3")
-    print(three_list)
-# Create a function that changes "s" to "$"
-def dollar_sign(term_list):
-    dollar_replace_list = term_list.copy()
-    for idx, value in enumerate(dollar_replace_list):
-        dollar_replace_list[idx] = value.replace("s", "$")
-    print(dollar_replace_list)
-# Create a function that changes "b" to "9"
-def nine_change(term_list):
-    nine_list = term_list.copy()
-    for idx, value in enumerate(nine_list):
-        nine_list[idx] = value.replace("b", "9")
-    print(nine_list)
-# Create a function that changes "l" to "1"
-def one_change(term_list):
-    one_list = term_list.copy()
-    for idx, value in enumerate(one_list):
-        one_list[idx] = value.replace("l", "1")
-    print(one_list)
-# Create a function that changes "r" to "4"
-def four_change(term_list):
-    four_list = term_list.copy()
-    for idx, value in enumerate(four_list):
-        four_list[idx] = value.replace("r", "4")
-    print(four_list)
-# Create a function that changes "i" to "1"
-def one_i_change(term_list):
-    one_i_list = term_list.copy()
-    for idx, value in enumerate(one_i_list):
-        one_i_list[idx] = value.replace("i", "4")
-    print(one_i_list)
-# Create a function that changes "t" to "7"
-def seven_change(term_list):
-    seven_list = term_list.copy()
-    for idx, value in enumerate(seven_list):
-        seven_list[idx] = value.replace("t", "7")
-    print(seven_list)
+        change_char_list.append(new_list[idx])
+    for elem in change_char_list:
+        master_list2.append(elem)
+    # print(change_char_list)
+
+
+# # Create a function that changes "a" to "@"
+# def a_change_at(term_list, old_value, new_value):
+#     new_list = term_list.copy()
+#     for idx, value in enumerate(new_list):
+#         new_list[idx] = value.replace(old_value, new_value)
+#     print(new_list)
+# # Create a function that changes "o" to "0"
+# def zero_change(term_list):
+#     zero_list = term_list.copy()
+#     for idx, value in enumerate(zero_list):
+#         zero_list[idx] = value.replace("o", "0")
+#     print(zero_list)
+# # Create a function that changes "e" to "3"
+# def three_change(term_list):
+#     three_list = term_list.copy()
+#     for idx, value in enumerate(three_list):
+#         three_list[idx] = value.replace("e", "3")
+#     print(three_list)
+# # Create a function that changes "s" to "$"
+# def dollar_sign(term_list):
+#     dollar_replace_list = term_list.copy()
+#     for idx, value in enumerate(dollar_replace_list):
+#         dollar_replace_list[idx] = value.replace("s", "$")
+#     print(dollar_replace_list)
+# # Create a function that changes "b" to "9"
+# def nine_change(term_list):
+#     nine_list = term_list.copy()
+#     for idx, value in enumerate(nine_list):
+#         nine_list[idx] = value.replace("b", "9")
+#     print(nine_list)
+# # Create a function that changes "l" to "1"
+# def one_change(term_list):
+#     one_list = term_list.copy()
+#     for idx, value in enumerate(one_list):
+#         one_list[idx] = value.replace("l", "1")
+#     print(one_list)
+# # Create a function that changes "r" to "4"
+# def four_change(term_list):
+#     four_list = term_list.copy()
+#     for idx, value in enumerate(four_list):
+#         four_list[idx] = value.replace("r", "4")
+#     print(four_list)
+# # Create a function that changes "i" to "1"
+# def one_i_change(term_list):
+#     one_i_list = term_list.copy()
+#     for idx, value in enumerate(one_i_list):
+#         one_i_list[idx] = value.replace("i", "1")
+#     print(one_i_list)
+# # Create a function that changes "t" to "7"
+# def seven_change(term_list):
+#     seven_list = term_list.copy()
+#     for idx, value in enumerate(seven_list):
+#         seven_list[idx] = value.replace("t", "7")
+#     print(seven_list)
 # Create a function that adds syms to end
 
 # Create a function that adds a sym to begaining
@@ -264,11 +316,57 @@ def seven_change(term_list):
 # Combine the lists again
 
 # Go through that final list and get rid of and element that has less than 6 charaters in it.
-
+# for elem in final_list:
+#     if len(elem) < 6:
+#         delete elem
+#     return final_list
 # output that list onto a file as a wordlist
 
 
+
 usr_input()
+print(master_list)
+
+for elem in master_list2:
+    print(elem)
+print(len(master_list))
+print(len(master_list2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 '''
